8f9d8c0237df4bbd7f1fbc2dfe5363afb6eeba47
Brian Hirsh <hirsheybar@fb.com>
2021-08-20 07:26:14 -0700

    generalize backward helper for multiple inputs

--- test/cpp/test_aten_xla_tensor.cpp
+++ test/cpp/test_aten_xla_tensor.cpp
@@ -3021,7 +3021,7 @@ TEST_F(AtenXlaTensorTest, TestSign) {
     torch::Tensor xla_a = CopyToDevice(a, device, /*requires_grad=*/true);
     torch::Tensor xla_b = torch::sign(xla_a);
     AllClose(b, xla_b);
-    AssertBackward(xla_b, xla_a, b, a);
+    AssertBackward(xla_b, {xla_a}, b, {a});
   });
   ExpectCounterNotChanged("aten::.*", cpp_test::GetIgnoredCounters());
 }
@@ -3045,7 +3045,7 @@ TEST_F(AtenXlaTensorTest, TestAbs) {
     torch::Tensor xla_a = CopyToDevice(a, device, /*requires_grad=*/true);
     torch::Tensor xla_b = torch::abs(xla_a);
     AllClose(b, xla_b);
-    AssertBackward(xla_b, xla_a, b, a);
+    AssertBackward(xla_b, {xla_a}, b, {a});
   });
   ExpectCounterNotChanged("aten::.*", cpp_test::GetIgnoredCounters());
 }
